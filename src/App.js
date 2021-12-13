aa6e71c8b9fc648edae59b8118c6fc17ac591038
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-12-13 11:26:55 -0800

    created index.js for image folder and passed down needed images to navBar component via props. Image successfully loaded

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React  from 'react';
 import {NavBar, Body, Footer} from './component'
 import {navBarLinks, serviceCards, footerLinks} from './data'
-
+// import magnifyingGlass from './images/magnifyingGlass.svg'
+import { images } from './images';
 const App = () => {
 
   return (
     <div className="App">
-      <NavBar navBarLinks={navBarLinks}/>
+      <NavBar navBarLinks={navBarLinks} images={images}/>
       <Body serviceCards={serviceCards}/>
       <Footer footerLinks={footerLinks} />
 
