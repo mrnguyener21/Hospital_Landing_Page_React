6a2c829bb6f448bda36aff4e8f9cc8782269704e
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-12-13 11:46:31 -0800

    imported over navBar html tags from vanilla JS project and refactored for JSX

--- src/component/NavBar/NavBar.js
+++ src/component/NavBar/NavBar.js
@@ -3,18 +3,23 @@ import styles from './NavBar.module.css'
 const NavBar = ({navBarLinks, images:{magnifyingGlass, logIn, hamburgerMenu}}) => {
     console.log(navBarLinks)
     return (
-        <div className={styles.navBarContainer}>
+        <div className={styles.navBar}>
             <h1 className={styles.logo}>PROJECT HOSPITAL</h1>
 
             <div className={styles.navBarLinksContainer}>
-                {navBarLinks.map(link => <h1 className={styles.link}>{link}</h1>)}
+                {navBarLinks.map(navBarLink => <h1 className={styles.navBarLink}>{navBarLink}</h1>)}
             </div>
-
-            <div className={styles.searchContainer}>
-                <input type="text" placeholder="Search..." className={styles.searchBar}/>
-                <button type='button' className={styles.searchButton}>
-                    <img src = {magnifyingGlass} alt="magnifying Glass SVG" class='searchImage'/>
-                </button>
+            <div className={styles.searchAndLoginContainer}>
+                <div className={styles.searchContainer}>
+                    <input type="text" placeholder="Search..." className={styles.searchBar}/>
+                    <button type='button' className={styles.searchButton}>
+                        <img src = {magnifyingGlass} alt="magnifying Glass SVG" className={styles.searchImage}/>
+                    </button>
+                </div>
+                <div className={styles.logInContainer}>
+                    <img src={logIn} alt="logIn SVG" className={styles.logInImage}/>
+                    <div className={styles.logInText}>login</div>
+                </div>
             </div>
         </div>
     )
