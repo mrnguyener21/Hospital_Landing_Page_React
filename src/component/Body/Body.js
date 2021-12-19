4824e6af477079bb5760c0400ed778781c96e473
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-12-19 11:35:44 -0800

    roughly added containers to the card section and rough copy * pasted over css style from vanilla JS landing page

--- src/component/Body/Body.js
+++ src/component/Body/Body.js
@@ -2,15 +2,19 @@ import styles from './Body.module.css'
 const Body = ({serviceCards}) => {
     console.log(serviceCards);
     return (
-        <div className={styles.card}>
-            {serviceCards.map(({image, description, button}) =>
-                <> 
-                <img src = {image} alt={"Care Center SVG"} className={styles.cardImage}/>
-                <h1>{description}</h1>
-                <button>{button}</button>
-                </>
-                
-             )}
+        <div className={styles.cardSection}>
+            <h1 className={styles.cardTitle}>Services We Offer</h1>
+            <div className={styles.cardContainer}>
+                <div className={styles.card}>
+                    {serviceCards.map(({image, description, button}) =>
+                        <> 
+                        <img src = {image} alt={"Care Center SVG"} className={styles.cardImage}/>
+                        <h1>{description}</h1>
+                        <button>{button}</button>
+                        </>
+                    )}
+                </div>
+            </div>
         </div>
     )
 }
